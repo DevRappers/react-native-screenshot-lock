c27aea5ea877c11933de6a10b03f5545bc0fc3b0
DevRappers <devrappers@gmail.com>
2020-03-03 15:55:19 +0900

    code change

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
 import { useEffect } from 'react';
-import { register, unregister } from './lockScreenShot';
+import { NativeModules, NativeEventEmitter, Platform, Alert } from 'react-native';
+
+const { ScreenshotLock } = NativeModules;
+
+const SCREENSHOT_EVENT = 'ScreenshotTaken';
+
+const eventEmitter = new NativeEventEmitter(ScreenshotLock);
+
+export const register = cb => {
+    eventEmitter.addListener(
+        SCREENSHOT_EVENT,
+        cb || (() => {
+            Alert.alert(
+                'Contenu protégé !',
+                "Ce contenu est protégé par les droits d'auteur. Tout partage en dehors d'un usage personnel est strictement interdit."
+            );
+        }),
+        {}
+    );
+    if (Platform.OS === 'android') {
+        ScreenshotLock.screenLock();
+    }
+};
+
+export const unregister = () => {
+    eventEmitter.removeAllListeners(SCREENSHOT_EVENT);
+    if (Platform.OS === 'android') {
+        ScreenshotLock.screenUnLock();
+    }
+};
 
 const useLockScreenShot = () => {
     useEffect(() => {

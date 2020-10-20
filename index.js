aa2887edc31b398d37d442979d9a56add3462e39
DevRappers <devrappers@gmail.com>
2020-10-20 17:08:01 +0900

    [FIX] receive message alert

--- index.js
+++ index.js
@@ -1,40 +1,48 @@
-import { useEffect } from 'react';
-import { NativeModules, NativeEventEmitter, Platform, Alert } from 'react-native';
+import { useEffect } from "react";
+import {
+  NativeModules,
+  NativeEventEmitter,
+  Platform,
+  Alert
+} from "react-native";
 
 const { ScreenshotLock } = NativeModules;
 
-const SCREENSHOT_EVENT = 'ScreenshotTaken';
+const SCREENSHOT_EVENT = "ScreenshotTaken";
+const DEFAULT_TITLE = "Contenu protégé !";
+const DEFAULT_MESSAGE =
+  "Ce contenu est protégé par les droits d'auteur. Tout partage en dehors d'un usage personnel est strictement interdit.";
 
 const eventEmitter = new NativeEventEmitter(ScreenshotLock);
 
-export const register = cb => {
-    eventEmitter.addListener(
-        SCREENSHOT_EVENT,
-        cb || (() => {
-            Alert.alert(
-                'Contenu protégé !',
-                "Ce contenu est protégé par les droits d'auteur. Tout partage en dehors d'un usage personnel est strictement interdit."
-            );
-        }),
-        {}
-    );
-    if (Platform.OS === 'android') {
-        ScreenshotLock.screenLock();
-    }
+export const register = (title, message) => {
+  eventEmitter.addListener(
+    SCREENSHOT_EVENT,
+    () => {
+      Alert.alert(title, message);
+    },
+    {}
+  );
+  if (Platform.OS === "android") {
+    ScreenshotLock.screenLock();
+  }
 };
 
 export const unregister = () => {
-    eventEmitter.removeAllListeners(SCREENSHOT_EVENT);
-    if (Platform.OS === 'android') {
-        ScreenshotLock.screenUnLock();
-    }
+  eventEmitter.removeAllListeners(SCREENSHOT_EVENT);
+  if (Platform.OS === "android") {
+    ScreenshotLock.screenUnLock();
+  }
 };
 
-const useLockScreenShot = () => {
-    useEffect(() => {
-        register();
-        return () => unregister();
-    }, []);
+const useLockScreenShot = ({
+  title = DEFAULT_TITLE,
+  message = DEFAULT_MESSAGE
+}) => {
+  useEffect(() => {
+    register(title, message);
+    return () => unregister();
+  }, []);
 };
 
 export default useLockScreenShot;
